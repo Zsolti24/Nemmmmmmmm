b8a55c16364c56cea4e7a5d7a577c9ce6e769e50
ZsoltiiiHUN <68284497+ZsoltiiiHUN@users.noreply.github.com>
2021-09-02 16:34:36 +0200

    Add files via upload

---
+++ FingersMusic-main/Nyilas game 1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Nyilas_game_1
+{
+    public partial class Form1 : Form
+    {
+
+        private SoundPlayer _soundPlayer;
+        public Form1()
+        {
+            InitializeComponent();
+            _soundPlayer = new SoundPlayer("i.wav");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            _soundPlayer.Play();
+            MessageBox.Show("asd");
+            timer1.Enabled = Enabled;
+        }
+    }
+}
+
